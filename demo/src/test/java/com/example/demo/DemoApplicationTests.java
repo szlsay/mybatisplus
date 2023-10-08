761163557e36c78787500592b2e825480a35917c
小沈曰 <409178030@qq.com>
2023-10-08 21:19:06 +0800

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,12 @@
 package com.example.demo;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
-import org.apache.ibatis.annotations.Param;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.Serializable;
 import java.util.*;
 
 @SpringBootTest
@@ -18,6 +14,36 @@ class DemoApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
+
+    /**
+     * 根据 ID 修改
+     * entity 实体对象
+     */
+//    int updateById(@Param(Constants.ENTITY) T entity);
+    @Test
+    void updateById() {
+        User user = new User();
+        user.setId(10L);
+        user.setName("A");
+        int number = userMapper.updateById(user);
+        System.out.println(number);
+    }
+
+    /**
+     * 根据 whereEntity 条件，更新记录
+     * entity        实体对象 (set 条件值,可以为 null)
+     * updateWrapper 实体对象封装操作类（可以为 null,里面的 entity 用于生成 where 语句）
+     */
+//    int update(@Param(Constants.ENTITY) T entity, @Param(Constants.WRAPPER) Wrapper<T> updateWrapper);
+    @Test
+    void update() {
+        User user = new User();
+        user.setId(10L);
+        user.setName("A");
+        int number = userMapper.updateById(user);
+        System.out.println(number);
+    }
+
     @Test
     void contextLoads() {
         List<User> users = userMapper.selectList(null);
@@ -26,8 +52,7 @@ class DemoApplicationTests {
 
     /**
      * 根据 ID 删除
-     *
-     * @param id 主键ID
+     * id 主键ID
      */
 //    int deleteById(Serializable id);
     @Test
@@ -38,8 +63,7 @@ class DemoApplicationTests {
 
     /**
      * 根据实体(ID)删除
-     *
-     * @param entity 实体对象
+     * entity 实体对象
      * @since 3.4.4
      */
 //    int deleteById(T entity);
@@ -52,8 +76,7 @@ class DemoApplicationTests {
     }
     /**
      * 根据 columnMap 条件，删除记录
-     *
-     * @param columnMap 表字段 map 对象
+     * columnMap 表字段 map 对象
      */
 //    int deleteByMap(@Param(Constants.COLUMN_MAP) Map<String, Object> columnMap);
     @Test
@@ -66,8 +89,7 @@ class DemoApplicationTests {
     }
     /**
      * 根据 entity 条件，删除记录
-     *
-     * @param queryWrapper 实体对象封装操作类（可以为 null,里面的 entity 用于生成 where 语句）
+     * queryWrapper 实体对象封装操作类（可以为 null,里面的 entity 用于生成 where 语句）
      */
 //    int delete(@Param(Constants.WRAPPER) Wrapper<T> queryWrapper);
 
@@ -81,8 +103,7 @@ class DemoApplicationTests {
 
     /**
      * 删除（根据ID或实体 批量删除）
-     *
-     * @param idList 主键ID列表或实体列表(不能为 null 以及 empty)
+     * idList 主键ID列表或实体列表(不能为 null 以及 empty)
      */
 //    int deleteBatchIds(@Param(Constants.COLL) Collection<?> idList);
     @Test
